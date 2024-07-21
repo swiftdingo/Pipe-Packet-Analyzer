4ed8d662807828e6f00f1c522c5dfe28a0f74196
Dan <daniel.d.cummings@gmail.com>
2024-07-21 15:11:26 -0700

    Add files via upload
    second commit. added input

--- pipe.c
+++ pipe.c
@@ -3,7 +3,7 @@
 
 #define MAXSIZE 256
 	
-int main() {
+int main(int argc, char *argv[]) {
 	
 	/*HANDLE serverPipe = CreateNamedPipe(
     L"\\\\.\\pipe\\mojo.external_task_manager_8396",	// name of our pipe, must be in the form of \\.\pipe\<NAME>
@@ -19,14 +19,20 @@ int main() {
 	HANDLE hFile;
 	CHAR Buf[MAXSIZE] = {0};
 	DWORD nIn = 0;
+	CHAR msg[MAXSIZE] = {0};
+	DWORD bytesWritten = 0;
 	
-
 	hFile = CreateFileA("\\\\.\\Pipe\\slobs", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
 		printf("\n File open error\n");
 	}
+	
+	// write the first variable as a message to the 
+	//msg = argv[0];
+	
+	WriteFile(hFile, argv[0], sizeof(msg), &bytesWritten, NULL);
 	
 	if (ReadFile(hFile, Buf, MAXSIZE - 2, &nIn, NULL) == FALSE)
 	{
@@ -36,22 +42,27 @@ int main() {
 	}
 	else
 		while(1)
-		if(nIn > 0 && nIn < MAXSIZE - 2)
 		{
-			printf("[+] %d bytes read from BUF\n", nIn);  
+			if(nIn > 0 && nIn < MAXSIZE - 2)
+			{
+				printf("[+] %d bytes read from BUF\n", nIn);  
+				printf("%s", &Buf);
+			}
+			else if (nIn == 0)
+			{
+				printf("No data to read from BUF\n");
+			}
+			
+			
+			// get users input data to write to the pipe
+			scanf("[+] DATA to write: %s", &msg);
+			
+			WriteFile(hFile, msg, sizeof(msg), &bytesWritten, NULL);
+			
 		}
-		else if (nIn == 0)
-		{
-			printf("No data to read from BUF\n");
-		}
-	
-	WriteFile(hFile, message, messageLength, &bytesWritten, NULL);
-		
-	printf("%s", &Buf);
-
+			
 	CloseHandle(hFile);
 	
-
 	return 0;
 	
 }
